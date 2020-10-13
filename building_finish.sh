3c478536cddc48c507f74c5b07fde05aa8005ad2
Ilya <mymail@lol.by>
2020-10-13 18:15:12 +0300

    One more attemption

--- building_finish.sh
+++ building_finish.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 . configs
-START_DIR=($PWD)
+START_DIR="$PWD"
 #Clone the repo
-
-cd $DIR_TO_INSTALL || { echo "Path isn't exist"; exit 1; }
+cd $DIR_TO_BUILD || { echo "Path isn't exist"; exit 1; }
 git clone https://github.com/TelegramMessenger/MTProxy
- cd $DIR_TO_INSTALL/MTProxy || { echo "Path isn't exist"; exit 1; }
+ cd $DIR_TO_BUILD/MTProxy || { echo "Path isn't exist"; exit 1; }
 #Building
-[[ -e "$DIR_TO_INSTALL/MTProxy/objs/bin/mtproto-proxy" ]] && systemctl stop MTProxy.service  && make clean && echo "CLEAN!"
+[[ -e "$DIR_TO_INSTALL/mtproto-proxy" ]] && systemctl stop MTProxy.service && echo "SERVICE WAS STOPPED!"
 if ! make ; then
 echo "Proxy didn't make" 
 make clean
+systemctl start MTProxy.service
 exit 1
  else echo "Proxy made" 
 fi
+mkdir "$DIR_TO_INSTALL" && cp "$DIR_TO_BUILD/MTProxy/objs/bin/mtproto-proxy" "$DIR_TO_INSTALL/mtproto-proxy" \
+ || { echo "Problem with installation"; exit 1; }
 #---------------------------------------------------------------
-cd $DIR_TO_INSTALL/MTProxy/objs/bin || { echo "Path  isn't exist"; exit 1; }
-curl -s https://core.telegram.org/getProxySecret -o $DIR_TO_INSTALL/MTProxy/objs/bin/proxy-secret
-curl -s https://core.telegram.org/getProxyConfig -o $DIR_TO_INSTALL/MTProxy/objs/bin/proxy-multi.conf
+cd $DIR_TO_INSTALL
+curl -s https://core.telegram.org/getProxySecret -o $DIR_TO_INSTALL/proxy-secret
+curl -s https://core.telegram.org/getProxyConfig -o $DIR_TO_INSTALL/proxy-multi.conf
 #---------------------------------------------------------------
 #creating cron rule
 [[ ! -e "/etc/cron.daily/autoproxydaily" ]] && echo "#!/bin/bash
@@ -26,32 +28,17 @@ cd $START_DIR
 chmod 755 /etc/cron.daily/autoproxydaily  && echo "CRON CREATED"
 #---------------------------------------------------------------
 # Systemd configuration
+  cp "$START_DIR"/MTProxy.service.inst "$DIR_TO_BUILD"/MTProxy
   [[ ! -e "etc/systemd/system/MTProxy.service" ]] && \
-  sed -i "s|\$DIR_TO_INSTALL|$DIR_TO_INSTALL|g" "$START_DIR"/MTProxy.service.inst && \
-  sed -i "s|\$USER_NM|$USER_NM|g" "$START_DIR"/MTProxy.service.inst && \
-  sed -i "s|\$L_PORT|$L_PORT|g" "$START_DIR"/MTProxy.service.inst && \
-  sed -i "s|\$USR_PORT|$USR_PORT|g" "$START_DIR"/MTProxy.service.inst && \
-  sed -i "s|\$SECRET|$SECRET|g" "$START_DIR"/MTProxy.service.inst && \
-  sed -i "s|\$WORKS|$WORKS|g" "$START_DIR"/MTProxy.service.inst && \
- # touch /etc/systemd/system/MTProxy.service && \
-  cp "$START_DIR/MTProxy.service.inst" /etc/systemd/system/MTProxy.service && \
+  sed -i "s|\$DIR_TO_INSTALL|$DIR_TO_INSTALL|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
+  sed -i "s|\$USER_NM|$USER_NM|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
+  sed -i "s|\$L_PORT|$L_PORT|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
+  sed -i "s|\$USR_PORT|$USR_PORT|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
+  sed -i "s|\$SECRET|$SECRET|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
+  sed -i "s|\$WORKS|$WORKS|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
+  cp "$DIR_TO_BUILD/MTProxy/MTProxy.service.inst" /etc/systemd/system/MTProxy.service && \
   chmod 664 /etc/systemd/system/MTProxy.service && \
   echo "SERVICE CREATED"
-
- # echo "[Unit]
-#Description=MTProxy
-#After=network.target
-
-#[Service]
-#Type=simple
-#WorkingDirectory=$DIR_TO_INSTALL/MTProxy/objs/bin/
-#ExecStart=$DIR_TO_INSTALL/MTProxy/objs/bin/mtproto-proxy -u $USER_NM $L_PORT -H $USR_PORT -S "$SECRET" \
-# --aes-pwd proxy-secret proxy-multi.conf -M $WORKS
-#Restart=on-failure
-
-#[Install]
-#WantedBy=multi-user.target" > /etc/systemd/system/MTProxy.service && chmod 664 /etc/systemd/system/MTProxy.service && \
-#echo "SERVICE CREATED"
 #---------------------------------------------------------------
 #starting service
 systemctl daemon-reload

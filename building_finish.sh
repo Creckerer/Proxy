2d24a61549a7804ba6baf59f18e6f9d004574181
Ilya <mymail@lol.by>
2020-10-13 20:29:56 +0300

    add Git pull

--- building_finish.sh
+++ building_finish.sh
@@ -2,20 +2,19 @@
 . configs
 START_DIR="$PWD"
 #Clone the repo
+mkdir "$DIR_TO_BUILD"
 cd $DIR_TO_BUILD || { echo "Path isn't exist"; exit 1; }
-git clone https://github.com/TelegramMessenger/MTProxy
- cd $DIR_TO_BUILD/MTProxy || { echo "Path isn't exist"; exit 1; }
+git init && git pull https://github.com/TelegramMessenger/MTProxy
 #Building
 [[ -e "$DIR_TO_INSTALL/mtproto-proxy" ]] && systemctl stop MTProxy.service && echo "SERVICE WAS STOPPED!"
 if ! make ; then
 echo "Proxy didn't make" 
 make clean
-systemctl start MTProxy.service
+systemctl start MTProxy.service  && echo "SERVICE STARTED!"
 exit 1
  else echo "Proxy made" 
 fi
-mkdir "$DIR_TO_INSTALL" && cp "$DIR_TO_BUILD/MTProxy/objs/bin/mtproto-proxy" "$DIR_TO_INSTALL/mtproto-proxy" \
- || { echo "Problem with installation"; exit 1; }
+mkdir "$DIR_TO_INSTALL" && cp "$DIR_TO_BUILD/objs/bin/mtproto-proxy" "$DIR_TO_INSTALL/mtproto-proxy"
 #---------------------------------------------------------------
 cd $DIR_TO_INSTALL
 curl -s https://core.telegram.org/getProxySecret -o $DIR_TO_INSTALL/proxy-secret
@@ -28,15 +27,15 @@ cd $START_DIR
 chmod 755 /etc/cron.daily/autoproxydaily  && echo "CRON CREATED"
 #---------------------------------------------------------------
 # Systemd configuration
-  cp "$START_DIR"/MTProxy.service.inst "$DIR_TO_BUILD"/MTProxy
+  cp "$START_DIR/MTProxy.service.inst" "$DIR_TO_BUILD"
   [[ ! -e "etc/systemd/system/MTProxy.service" ]] && \
-  sed -i "s|\$DIR_TO_INSTALL|$DIR_TO_INSTALL|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
-  sed -i "s|\$USER_NM|$USER_NM|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
-  sed -i "s|\$L_PORT|$L_PORT|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
-  sed -i "s|\$USR_PORT|$USR_PORT|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
-  sed -i "s|\$SECRET|$SECRET|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
-  sed -i "s|\$WORKS|$WORKS|g" "$DIR_TO_BUILD"/MTProxy/MTProxy.service.inst && \
-  cp "$DIR_TO_BUILD/MTProxy/MTProxy.service.inst" /etc/systemd/system/MTProxy.service && \
+  sed -i "s|\$DIR_TO_INSTALL|$DIR_TO_INSTALL|g" "$DIR_TO_BUILD"/MTProxy.service.inst && \
+  sed -i "s|\$USER_NM|$USER_NM|g" "$DIR_TO_BUILD"/MTProxy.service.inst && \
+  sed -i "s|\$L_PORT|$L_PORT|g" "$DIR_TO_BUILD"/MTProxy.service.inst && \
+  sed -i "s|\$USR_PORT|$USR_PORT|g" "$DIR_TO_BUILD"/MTProxy.service.inst && \
+  sed -i "s|\$SECRET|$SECRET|g" "$DIR_TO_BUILD"/MTProxy.service.inst && \
+  sed -i "s|\$WORKS|$WORKS|g" "$DIR_TO_BUILD"/MTProxy.service.inst && \
+  cp "$DIR_TO_BUILD/MTProxy.service.inst" /etc/systemd/system/MTProxy.service && \
   chmod 664 /etc/systemd/system/MTProxy.service && \
   echo "SERVICE CREATED"
 #---------------------------------------------------------------

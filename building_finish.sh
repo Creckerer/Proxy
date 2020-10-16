31cab384a3916b33af04626d50e9fe1c027cb8f9
Ilya <mymail@lol.by>
2020-10-16 14:42:11 +0300

    Change basename and logic

--- building_finish.sh
+++ building_finish.sh
@@ -8,7 +8,8 @@ else
   mkdir "$DIR_TO_BUILD"
   cd $DIR_TO_BUILD
   git clone https://github.com/TelegramMessenger/MTProxy
-  cd "$DIR_TO_BUILD/MTProxy" || { echo "Path isn't exist"; exit 1; }
+  cd "$DIR_TO_BUILD/MTProxy" || \
+    { echo "Path isn't exist"; exit 1; }
 fi
 #---------------------------------------------------------------
 #Building
@@ -19,6 +20,7 @@ if ! make ; then
 else
   echo "BUILD DONE"
 fi
+#---------------------------------------------------------------
 if [ -e "$DIR_TO_INSTALL/mtproto-proxy" ]; then
   systemctl stop MTProxy.service
   echo "SERVICE STOPPED FOR A LITTLE"
@@ -29,32 +31,32 @@ else
 fi
 #---------------------------------------------------------------
 if [ -e "$DIR_TO_INSTALL/proxy-secret" ] && [ -e "$DIR_TO_INSTALL/proxy-multi.conf" ]
-  if [ ! -d "$DIR_TO_INSTALL/MTProxy_install_bak" ];
-    then mkdir "$DIR_TO_INSTALL/MTProxy_install_bak"
-  fi
 then
-  cp "$DIR_TO_INSTALL/proxy-secret" "$DIR_TO_INSTALL/MTProxy_install_bak"
-  cp "$DIR_TO_INSTALL/proxy-multi.conf" "$DIR_TO_INSTALL/MTProxy_install_bak"
+  [ ! -d "$DIR_TO_INSTALL/MTProxy_install_bak" ] && \
+    mkdir "$DIR_TO_INSTALL/MTProxy_install_bak"
+  [ -d "$DIR_TO_INSTALL/MTProxy_install_bak" ] && \
+    cp "$DIR_TO_INSTALL/proxy-secret" "$DIR_TO_INSTALL/MTProxy_install_bak" && \
+    cp "$DIR_TO_INSTALL/proxy-multi.conf" "$DIR_TO_INSTALL/MTProxy_install_bak"
+  elif [ ! -e "$DIR_TO_INSTALL/proxy-secret" ] || [ ! -e "$DIR_TO_INSTALL/proxy-multi.conf" ]
+then
   curl -s https://core.telegram.org/getProxySecret -o "$DIR_TO_INSTALL/proxy-secret" && \
-    curl -s https://core.telegram.org/getProxyConfig -o "$DIR_TO_INSTALL/proxy-multi.conf" || \
-    echo "CHECK CONFIG FILES"
-else
-    curl -s https://core.telegram.org/getProxySecret -o "$DIR_TO_INSTALL/proxy-secret" && \
     curl -s https://core.telegram.org/getProxyConfig -o "$DIR_TO_INSTALL/proxy-multi.conf" || \
     echo "CHECK CONFIG FILES"
 fi
 #---------------------------------------------------------------
 #creating cron rule
-if [[ ! -e "/etc/cron.daily/autoproxydaily" ]]; then
+if [ ! -e "/etc/cron.daily/autoproxydaily" ]; then
   cp "$START_DIR/cron_autoproxydaily" "$DIR_TO_BUILD"
-  sed -i "s|START_DIR|$START_DIR|g" "$DIR_TO_BUILD/cron_autoproxydaily"
-  cp "$DIR_TO_BUILD/cron_autoproxydaily" "/etc/cron.daily/autoproxydaily"
+  sed -i "s|START_DIR|$START_DIR|g" "$DIR_TO_BUILD/cron_autoproxydaily" && \
+  sed -i "s|NAME_OF_SCRIPT|$0|g" "$DIR_TO_BUILD/cron_autoproxydaily"
+  cp "$DIR_TO_BUILD/cron_autoproxydaily" "/etc/cron.daily/autoproxydaily" || \
+    echo "CHECK CRON RULE"
   chmod 755 "/etc/cron.daily/autoproxydaily"
   echo "CRON CREATED"
 fi
 #---------------------------------------------------------------
 # Systemd configuration
-if [[ ! -e "etc/systemd/system/MTProxy.service" ]]; then
+if [ ! -e "etc/systemd/system/MTProxy.service" ]; then
   cp "$START_DIR/MTProxy.service.inst" "$DIR_TO_BUILD"
   sed -i "s|DIR_TO_INSTALL|$DIR_TO_INSTALL|g" "$DIR_TO_BUILD/MTProxy.service.inst" && \
     sed -i "s|USER_NM|$USER_NM|g" "$DIR_TO_BUILD/MTProxy.service.inst" && \
@@ -70,6 +72,7 @@ fi
 #starting service
 systemctl daemon-reload
 systemctl restart MTProxy.service
-systemctl status MTProxy.service || { echo "Problem with MTProxy.service"; exit 1; }
+systemctl status MTProxy.service || \
+  { echo "Problem with MTProxy.service"; exit 1; }
 systemctl enable MTProxy.service
 #---------------------------------------------------------------
